76738f29d4452feb221f2c8052d1604caf4a9719
Priyanshu Jash <128898002+SuperCoderPrij@users.noreply.github.com>
2026-09-12 23:39:31 +0530

    Display welcome message with logged-in user's name
    Fetch logged-in user data from local storage and display name.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,11 @@ import { removeUser } from "../features/authSlice";
 
 const HomePage = () => {
   let dispatch = useDispatch();
+  let LSseFetchKiyaHuaData = JSON.parse(localStorage.getItem("loggedInUser");
   return (
     <div className="h-screen bg-black text-white text-6xl font-semibold flex flex-col items-center justify-center">
       This is test home.
+      Welcome Back {LSseFetchKiyaHuaData.name}!
       <div
         className="m-3 px-4 py-2 text-3xl rounded-lg bg-red-700 cursor-pointer"
         onClick={() => dispatch(removeUser())}
